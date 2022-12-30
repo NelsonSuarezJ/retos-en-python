a965e5702fec9149a91ba47ed68bb851a9c335bc
Nelson <nelsonsuarezj@gmail.com>
2022-12-30 17:21:03 -0500

    correccion reto 2

--- py_002.py
+++ py_002.py
@@ -1,11 +1,12 @@
 """
 Escribe un programa que pueda calcular el factorial de un número dado. 
 Los resultados deben imprimirse en una secuencia separada por comas en una sola línea. 
-Supongamos que se suministra la siguiente entrada al programa: 8 Entonces, la salida debería ser: 40320
+Supongamos que se suministra la siguiente entrada al programa: 8 Entonces,
+la salida debería ser: 40320
 """
 def factorial(fact):
     if fact==1:
-        return
+        return 1
     return fact*factorial(fact-1) 
 
 fact=int(input("introduzca el factorial: "))
